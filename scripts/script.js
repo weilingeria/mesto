46d21fb3116ad8a73ce89ffa33c9ea8a6eeeca2e
weilingeria <valeria.natsibulina@yandex.ru>
2022-05-14 01:03:20 +0300

    Исправить замечания после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -8,43 +8,29 @@ const formElement = popup.querySelector('.popup__form');
 const nameInput = popup.querySelector('.popup__input_type_name');
 const jobInput = popup.querySelector('.popup__input_type_description');
 
-//Функция: открывает окно "Редактировать профиль" и вносит туда сразу инфу из граф имя и описание
-function popupOpen() {
-    popup.classList.add('popup_is-opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileDescription.textContent;
-  }
-
-//Функция: открывает и закрывает окно
+//Функция: открывает и закрывает окно "Редактировать профиль", при открытии сразу вносит в инпуты инфу из граф имя и описание
 function popupOpenToggle() {
     popup.classList.toggle('popup_is-opened');
+
+    if (popup.classList.contains('popup_is-opened')) {
+      nameInput.value = profileName.textContent;
+      jobInput.value = profileDescription.textContent;
+    }
   }
 
-//Функция: отправляет новые введенные данные в графы имени и описания
+//Функция: отправляет новые введенные данные в графы имени и описания и закрывает форму
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
-}
-
-//Функция: закрывает окно при нажатии на overlay
-function popupOverlayClickHadler(evt) {
-  if (evt.target === evt.currentTarget) {
-    popupOpenToggle();
-  }
+    popupOpenToggle()
 }
 
 //Открыть окно, нажав на кнопку "Редактировать профиль"
-openPopupButton.addEventListener('click', popupOpen);
+openPopupButton.addEventListener('click', popupOpenToggle);
 
 //Закрыть окно "Редактрировать профиль", нажав на крестик
 closePopupButton.addEventListener('click', popupOpenToggle);
 
-//Закрыть окно, нажав на overlay
-popup.addEventListener('click', popupOverlayClickHadler);
-
 //Отправить новые данные в форму
-formElement.addEventListener('submit', formSubmitHandler); 
-
-//Закрыть окно, нажав на кнопку "Сохранить"
-saveInfoButton.addEventListener('click', popupOpenToggle);
+formElement.addEventListener('submit', formSubmitHandler);
