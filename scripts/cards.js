623e700301600214e313abc3db031398de960140
weilingeria <valeria.natsibulina@yandex.ru>
2022-06-09 17:22:46 +0300

    Поменять карточки и аватар

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
       name: '美美的海底🌊',
-      link: 'https://images.unsplash.com/photo-1603798125698-a35feca4ed6d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387'
+      link: 'https://images.unsplash.com/photo-1534179523731-b2922018150a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
       name: '抓水母',
-      link: 'https://images.unsplash.com/photo-1519747180378-d55bc9a23efd?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387'
+      link: 'https://images.unsplash.com/photo-1533066636271-fdbe3e84ad80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
       name: '⭐派大星⭐ My homie',
-      link: 'https://images.unsplash.com/photo-1588518008356-bbd53d98e410?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170'
+      link: 'https://images.unsplash.com/photo-1551443403-7404fe0e6255?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
       name: '菠萝房',
-      link: 'https://images.unsplash.com/photo-1458791087439-278afc90b1d5?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387'
+      link: 'https://images.unsplash.com/photo-1644258201768-e6d8b09e34a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
       name: '美味汉堡',
-      link: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699'
+      link: 'https://images.unsplash.com/photo-1506079478915-3f458c5077a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
       name: 'Gary 小蜗🐌',
-      link: 'https://images.unsplash.com/photo-1596708635238-aab8d249a455?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464'
+      link: 'https://images.unsplash.com/photo-1600103377176-1ee12e3458f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=403&q=80'
     }
   ];
