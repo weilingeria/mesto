4b262aa526dfdc9d4c38aea708af0833cd828935
weilingeria <valeria.natsibulina@yandex.ru>
2022-06-09 17:46:59 +0300

    Заменить имя и подписи

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: '美美的海底🌊',
+      name: '粉红房子🏠',
       link: 'https://images.unsplash.com/photo-1534179523731-b2922018150a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
-      name: '抓水母',
+      name: '孤独的仙人掌🌵',
       link: 'https://images.unsplash.com/photo-1533066636271-fdbe3e84ad80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
-      name: '⭐派大星⭐ My homie',
+      name: '美丽的大海🌊',
       link: 'https://images.unsplash.com/photo-1551443403-7404fe0e6255?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
-      name: '菠萝房',
+      name: '在棕榈的阴影下🥥',
       link: 'https://images.unsplash.com/photo-1644258201768-e6d8b09e34a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
-      name: '美味汉堡',
+      name: '天气真好，适合微笑🙂',
       link: 'https://images.unsplash.com/photo-1506079478915-3f458c5077a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
-      name: 'Gary 小蜗🐌',
+      name: '冰淇淋车🍦',
       link: 'https://images.unsplash.com/photo-1600103377176-1ee12e3458f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=403&q=80'
     }
   ];
